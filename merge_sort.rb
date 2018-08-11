cf8ee556e538075245956d41eb3804051d81633c
Oky Sabeni <osabeni@mavenlink.com>
2018-08-11 06:12:59 -0700

    WIP

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,16 +1,46 @@
 
 
-def merge_sort(numbers, left_number, right_number)
-  # return if numbers.size == 1
+def _merge_sort(left_numbers, right_numbers)
+  return merge(left_numbers, right_numbers) if numbers.size == 1 && right_numbers == 1
+
   # split the numbers into two
-  # numbers_left = numbers[0..numbers.length/2]
-  # numbers_right = numbers[numbers.length/2+1..numbers.length]
-  # merge_sort(numbers_left, left_number, right_number)
-  # merge_sort(numbers_right, left_number, right_number)
+  numbers_left = numbers_left[0..left_numbers.length/2]
+  numbers_right = numbers_right[left_numbers.length/2+1..left_numbers.length]
+
+  _merge_sort(numbers_left, numbers_right)
+  # _merge_sort(numbers_right, left_number, right_number)
   # merge(numbers_left, numbers_right)
 end
 
+@count = 7
+
+def merge_sort(numbers)
+  # @count -= 1
+  # exit(1) if @count == 1
+  puts "numbers: #{numbers}"
+  return numbers if numbers.size == 1
+
+  numbers_left = numbers[0...numbers.length/2]
+  numbers_right = numbers[numbers.length/2..numbers.length]
+
+  puts "numbers_left: #{numbers_left}"
+  puts "numbers_right: #{numbers_right}"
+
+  # if numbers_left.size == 1 && numbers_right.size == 1
+  #   merge(numbers_left, numbers_right)
+  # else
+  #
+  # end
+
+  merge_sort(numbers_left)
+  merge_sort(numbers_right)
+
+  results = merge(numbers_left, numbers_right)
+  results
+end
+
 def merge(array_one, array_two)
+  puts "merging: #{array_one}, #{array_two}"
   results = Array.new
 
   while(!array_one.empty? && !array_two.empty?)
@@ -25,7 +55,8 @@ def merge(array_one, array_two)
   results.push(*array_one) if array_two.empty?
 end
 
-input_one = [1,5,9,11]
-input_two = [2,4,8]
+input_one = [2,1]
+input_two = [12,5]
 puts merge(input_one, input_two).to_s
 
+puts merge_sort([2,1,12,5]).to_s
